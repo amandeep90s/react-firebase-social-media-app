855eb9ba3d00223cf917960c147bb9b332547d83
programmeramandeep <programmeramandeep@gmail.com>
2021-05-19 09:34:55 +0530

    set express and formatting

---
+++ dbSchema.js
@@ -0,0 +1,11 @@
+let db = {
+    screams: [
+        {
+            userHandle: "user",
+            body: "this is the scream body",
+            createdAt: "2021-05-19T02:59:17.968Z",
+            likeCount: 5,
+            commentCount: 10,
+        },
+    ],
+};
