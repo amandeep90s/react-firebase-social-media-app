2525911b09fef02d588c02e03f32ccb772aaca4b
programmeramandeep <programmeramandeep@gmail.com>
2021-05-19 09:46:33 +0530

    setup firebase config

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,21 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
-const express = require("express");
+const app = require("express")();
+require("dotenv").config();
 
 admin.initializeApp();
 
-const app = express();
+const firebase = require("firebase");
+const config = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+};
+
+firebase.initializeApp(config);
 
 app.get("/screams", (req, res) => {
     admin
