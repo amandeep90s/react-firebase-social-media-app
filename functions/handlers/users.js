a70efef7eb3c16d0a843702157b8e88d661314df
programmeramandeep <programmeramandeep@gmail.com>
2021-05-20 10:02:41 +0530

    refactoring and organising

---
+++ functions/handlers/users.js
@@ -0,0 +1,95 @@
+const { db } = require("../util/admin");
+const firebase = require("firebase");
+require("firebase/auth");
+const config = require("../util/config");
+
+const { validateLoginData, validateSignupData } = require("../util/validators");
+
+firebase.initializeApp(config);
+
+exports.signup = (req, res) => {
+    const newUser = {
+        email: req.body.email,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword,
+        handle: req.body.handle,
+    };
+
+    const { errors, valid } = validateSignupData(newUser);
+    if (!valid) return res.status(400).json(errors);
+
+    let token, userId;
+    db.doc(`/users/${newUser.handle}`)
+        .get()
+        .then((doc) => {
+            if (doc.exists) {
+                return res
+                    .status(400)
+                    .json({ handle: "This handle is already taken" });
+            } else {
+                return firebase
+                    .auth()
+                    .createUserWithEmailAndPassword(
+                        newUser.email,
+                        newUser.password
+                    );
+            }
+        })
+        .then((data) => {
+            userId = data.user.uid;
+            return data.user.getIdToken();
+        })
+        .then((tokenId) => {
+            token = tokenId;
+            const userCredentials = {
+                handle: newUser.handle,
+                email: newUser.email,
+                createdAt: new Date().toISOString(),
+                userId,
+            };
+
+            return db
+                .doc(`/users/${newUser.handle}`)
+                .set(userCredentials)
+                .then(() => {
+                    return res.status(201).json({ token });
+                })
+                .catch((error) => console.error(error));
+        })
+        .catch((error) => {
+            console.error(error);
+            if (error.code === "auth/email-already-in-use") {
+                return res
+                    .status(400)
+                    .json({ email: "Email is already in use" });
+            } else {
+                return res.status(500).json({ error: error.code });
+            }
+        });
+};
+
+exports.login = (req, res) => {
+    const user = {
+        email: req.body.email,
+        password: req.body.password,
+    };
+
+    const { errors, valid } = validateLoginData(user);
+    if (!valid) return res.status(400).json(errors);
+
+    firebase
+        .auth()
+        .signInWithEmailAndPassword(user.email, user.password)
+        .then((data) => {
+            return data.user.getIdToken();
+        })
+        .then((token) => {
+            return res.json({ token });
+        })
+        .catch((error) => {
+            console.error(error);
+            return res
+                .status(403)
+                .json({ general: "Wrong credentials, please try again" });
+        });
+};
---
+++ functions/util/validators.js
@@ -0,0 +1,48 @@
+const isEmpty = (string) => (string.trim() === "" ? true : false);
+
+const isEmail = (email) => {
+    const regEx =
+        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    return email.match(regEx) ? true : false;
+};
+
+exports.validateSignupData = (data) => {
+    // Validation
+    let errors = {};
+
+    if (isEmpty(data.email)) {
+        errors.email = "Must not be empty";
+    } else if (!isEmail(data.email)) {
+        errors.email = "Must be a valid email address";
+    }
+
+    if (isEmpty(data.password)) errors.password = "Must not be empty";
+    if (data.password !== data.confirmPassword) {
+        errors.confirmPassword = "Passwords must match";
+    }
+    if (isEmpty(data.handle)) errors.handle = "Must not be empty";
+
+    return {
+        errors,
+        valid: Object.keys(errors).length === 0 ? true : false,
+    };
+};
+
+exports.validateLoginData = (data) => {
+    // Validation
+    let errors = {};
+
+    if (isEmpty(data.email)) {
+        errors.email = "Must not be empty";
+    } else if (!isEmail(data.email)) {
+        errors.email = "Must be a valid email address";
+    }
+
+    if (isEmpty(data.password)) errors.password = "Must not be empty";
+
+    return {
+        errors,
+        valid: Object.keys(errors).length === 0 ? true : false,
+    };
+};
