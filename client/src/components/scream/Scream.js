70713d8af70090f9da92239d5f82b9681d5cbb97
programmeramandeep <programmeramandeep@gmail.com>
2021-05-21 21:44:47 +0530

    post card details

--- client/src/components/scream/Scream.js
+++ client/src/components/scream/Scream.js
@@ -1,7 +1,80 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import moment from "moment";
+import PropTypes from "prop-types";
 
-const Scream = () => {
-    return <div></div>;
+// MUI Stuff
+import {
+    Button,
+    Card,
+    CardActionArea,
+    CardActions,
+    CardContent,
+    CardMedia,
+    Typography,
+    makeStyles,
+} from "@material-ui/core";
+// Icons
+import CommentIcon from "@material-ui/icons/Comment";
+
+// Custom Style
+const useStyles = makeStyles({
+    card: {
+        position: "relative",
+        display: "flex",
+        marginBottom: 20,
+    },
+    content: {
+        padding: 25,
+        objectFit: "cover",
+    },
+    image: {
+        minWidth: 200,
+    },
+});
+
+const Scream = (props) => {
+    const classes = useStyles();
+    const {
+        body,
+        commentCount,
+        createdAt,
+        likeCount,
+        screamId,
+        userHandle,
+        userImage,
+    } = props.scream;
+
+    return (
+        <Card className={classes.card}>
+            <CardMedia
+                className={classes.image}
+                image={userImage}
+                title="User Profile"
+            />
+            <CardContent className={classes.content}>
+                <Typography
+                    gutterBottom
+                    variant="h5"
+                    color="primary"
+                    component={Link}
+                    to={`/users/${userHandle}`}
+                >
+                    {userHandle}
+                </Typography>
+                <Typography variant="body1" color="textSecondary" component="p">
+                    {moment(createdAt).fromNow()}
+                </Typography>
+                <Typography variant="body1" color="textSecondary" component="p">
+                    {body}
+                </Typography>
+            </CardContent>
+        </Card>
+    );
 };
 
 export default Scream;
+
+Scream.propTypes = {
+    scream: PropTypes.object.isRequired,
+};
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Grid } from "@material-ui/core";
+
+// Component
+import Scream from "../components/scream/Scream";
 
 const Home = () => {
-    return <div>Home</div>;
+    const [screams, setScreams] = useState([]);
+
+    const recentScreamsMarkup =
+        screams.length > 0 ? (
+            screams.map((scream) => (
+                <Scream scream={scream} key={scream.screamId} />
+            ))
+        ) : (
+            <p>Loading...</p>
+        );
+
+    useEffect(() => {
+        axios
+            .get("/screams")
+            .then((res) => {
+                setScreams(res.data);
+            })
+            .catch((error) => console.log(error));
+    }, []);
+
+    return (
+        <Grid container spacing={2}>
+            <Grid item sm={8} xs={12}>
+                {recentScreamsMarkup}
+            </Grid>
+            <Grid item sm={4} xs={12}>
+                <p>Profile</p>
+            </Grid>
+        </Grid>
+    );
 };
 
 export default Home;
