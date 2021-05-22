b5734903428fa15b073a6dceba71c3e394e2f1b0
programmeramandeep <programmeramandeep@gmail.com>
2021-05-22 13:10:02 +0530

    setup redux store

---
+++ client/src/redux/store.js
@@ -0,0 +1,29 @@
+import { applyMiddleware, combineReducers, compose, createStore } from "redux";
+import thunk from "redux-thunk";
+import dataReducer from "./reducers/dataReducer";
+import userReducer from "./reducers/userReducer";
+import uiReducer from "./reducers/uiReducer";
+
+// Initial State
+const initialState = {};
+
+// Middleware
+const middleware = [thunk];
+
+// Combine reducers
+const reducers = combineReducers({
+    user: userReducer,
+    data: dataReducer,
+    UI: uiReducer,
+});
+
+// Create Store
+const composeEnhancers =
+    typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+        ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+        : compose;
+const enhancer = composeEnhancers(applyMiddleware(...middleware));
+
+const store = createStore(reducers, initialState, enhancer);
+
+export default store;
